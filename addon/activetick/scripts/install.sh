759205d3b6956df95aa7a095f1ab5e8a694d1549
Sheldon Thomas <sthomas@corp.paltalk.com>
2015-10-12 16:43:29 -0400

    More install script

--- addon/activetick/scripts/install.sh
+++ addon/activetick/scripts/install.sh
@@ -1,6 +1,31 @@
 if ! type wget > /dev/null; then
   echo 'Please install wget'
+  exit 1
 fi
 
+# cd ..
+# mkdir import
+sysname=$(uname -s)
+arch=$(uname -m)
 
+case $( uname -s ) in
+Linux)
+  if [ "$arch" == "x86_64" ]
+  then
+    echo Linux64bit
+  else
+    echo Linux32bit
+  fi
+  ;;
+Darwin)
+  if [ "$arch" == "x86_64" ]
+  then
+    echo Mac64
+  else
+    echo Mac32
+  fi
+  ;;
+esac
+
+# echo $uname -s
 
