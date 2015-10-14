c634ad1ba49f1600828a07f11000bca23898d7d5
sheldon <sheldon>
2015-10-14 18:46:42 +0000

    Adjust script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,10 @@
 sysname=$(uname -s)
 arch=$(uname -m)
 
+cd protobuf
+./build.sh
+cd ..
+
 mkdir import
 cd import
 
@@ -8,9 +12,9 @@ case $( uname -s ) in
 Linux)
   if [ "$arch" == "x86_64" ]
   then
-    curl https://www.activetick.com/activetick/contents/binaries/atfeedapi/atfeed-cppsdk-ubuntu.x86_64.tar.gz | tar x
+    curl https://www.activetick.com/activetick/contents/binaries/atfeedapi/atfeed-cppsdk-ubuntu.x86_64.tar.gz | tar xz
   else
-    curl https://www.activetick.com/activetick/contents/binaries/atfeedapi/atfeed-cppsdk-ubuntu.i386.tar.gz | tar x
+    curl https://www.activetick.com/activetick/contents/binaries/atfeedapi/atfeed-cppsdk-ubuntu.i386.tar.gz | tar xz
   fi
   cp atfeed-cppsdk/bin/libActiveTickServerAPI.so ..
   ;;
@@ -19,9 +23,6 @@ Darwin)
   cp atfeed-cppsdk/bin/libActiveTickServerAPI.dylib ..
   ;;
 esac
-
-cd ..
-./protobuf/build.sh
 
 exit 0
 
